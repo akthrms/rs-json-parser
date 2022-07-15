9d56b175ba8a5270551ad6bfee5597f69ad7f4e1
akthrms <akt.hrms@gmail.com>
2022-07-15 11:42:20 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use nom::{branch::alt, bytes::complete::tag, IResult};
 use std::collections::HashMap;
 
+#[derive(Debug, PartialEq)]
 pub enum Json {
     String(String),
     Number(f64),
@@ -27,4 +28,27 @@ fn parse_boolean(input: &str) -> IResult<&str, Json> {
 fn parse_null(input: &str) -> IResult<&str, Json> {
     let (input, _) = tag("null")(input)?;
     Ok((input, Json::Null))
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{parse_boolean, parse_null, Json};
+
+    #[test]
+    fn test_parse_boolean() -> Result<(), Box<dyn std::error::Error>> {
+        let (_, json) = parse_boolean("true")?;
+        assert_eq!(json, Json::Boolean(true));
+
+        let (_, json) = parse_boolean("false")?;
+        assert_eq!(json, Json::Boolean(false));
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_parse_null() -> Result<(), Box<dyn std::error::Error>> {
+        let (_, json) = parse_null("null")?;
+        assert_eq!(json, Json::Null);
+        Ok(())
+    }
 }
