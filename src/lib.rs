fa77eb42207ac92d300c0bacfa3bca9f8a080769
akthrms <akt.hrms@gmail.com>
2022-07-25 17:31:08 +0900

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use nom::{
     branch::alt,
     bytes::complete::tag,
-    character::complete::{alphanumeric0, char, digit1, multispace0},
+    character::complete::{alpha1, alphanumeric0, alphanumeric1, char, digit1, multispace0},
     combinator::{eof, opt, recognize},
-    multi::separated_list0,
+    multi::{many0, separated_list0},
     sequence::{delimited, tuple},
     IResult,
 };
@@ -21,88 +21,72 @@ pub enum Json {
 
 impl Json {
     pub fn parse<'a>(input: &'a str) -> Result<Json, Box<dyn Error + 'a>> {
-        let (_, (json, _)) = tuple((alt((parse_array, parse_object)), eof))(input)?;
+        let (_, (json, _)) = tuple((alt((array, object)), eof))(input)?;
         Ok(json)
     }
 }
 
-fn parse_string(input: &str) -> IResult<&str, Json> {
-    let (input, s) = delimited(char('"'), alphanumeric0, char('"'))(input)?;
-    Ok((input, Json::String(s.to_string())))
+fn string(input: &str) -> IResult<&str, Json> {
+    let (input, value) = delimited(char('"'), alphanumeric0, char('"'))(input)?;
+    Ok((input, Json::String(value.to_string())))
 }
 
-fn parse_number(input: &str) -> IResult<&str, Json> {
-    fn parse_float(input: &str) -> IResult<&str, f64> {
-        let (input, n) = recognize(tuple((digit1, char('.'), digit1)))(input)?;
-        Ok((input, n.parse().unwrap()))
+fn number(input: &str) -> IResult<&str, Json> {
+    fn float(input: &str) -> IResult<&str, f64> {
+        let (input, value) = recognize(tuple((digit1, char('.'), digit1)))(input)?;
+        Ok((input, value.parse().unwrap()))
     }
 
-    fn parse_integer(input: &str) -> IResult<&str, f64> {
-        let (input, n) = digit1(input)?;
-        Ok((input, n.parse().unwrap()))
+    fn integer(input: &str) -> IResult<&str, f64> {
+        let (input, value) = digit1(input)?;
+        Ok((input, value.parse().unwrap()))
     }
 
-    let (input, op) = opt(char('-'))(input)?;
-    let (input, n) = alt((parse_float, parse_integer))(input)?;
-    Ok((input, Json::Number(if op.is_some() { -n } else { n })))
+    let (input, unary_minus) = opt(char('-'))(input)?;
+    let (input, value) = alt((float, integer))(input)?;
+    Ok((
+        input,
+        Json::Number(if unary_minus.is_some() { -value } else { value }),
+    ))
 }
 
-fn parse_boolean(input: &str) -> IResult<&str, Json> {
-    fn parse_true(input: &str) -> IResult<&str, Json> {
-        let (input, _) = tag("true")(input)?;
-        Ok((input, Json::Boolean(true)))
-    }
-
-    fn parse_false(input: &str) -> IResult<&str, Json> {
-        let (input, _) = tag("false")(input)?;
-        Ok((input, Json::Boolean(false)))
-    }
-
-    alt((parse_true, parse_false))(input)
+fn boolean(input: &str) -> IResult<&str, Json> {
+    let (input, value) = alt((tag("true"), tag("false")))(input)?;
+    Ok((input, Json::Boolean(value.parse().unwrap())))
 }
 
-fn parse_array(input: &str) -> IResult<&str, Json> {
-    fn parse_json_list(input: &str) -> IResult<&str, Vec<Json>> {
+fn array(input: &str) -> IResult<&str, Json> {
+    fn json_list(input: &str) -> IResult<&str, Vec<Json>> {
         let (input, json_list) = separated_list0(
             ws_char(','),
-            alt((
-                parse_string,
-                parse_number,
-                parse_boolean,
-                parse_array,
-                parse_object,
-                parse_null,
-            )),
+            alt((string, number, boolean, array, object, null)),
         )(input)?;
         Ok((input, json_list))
     }
 
-    let (input, json_list) = delimited(ws_char('['), parse_json_list, ws_char(']'))(input)?;
+    let (input, json_list) = delimited(ws_char('['), json_list, ws_char(']'))(input)?;
     Ok((input, Json::Array(json_list)))
 }
 
-fn parse_object(input: &str) -> IResult<&str, Json> {
-    fn parse_key_value(input: &str) -> IResult<&str, (String, Json)> {
-        let (input, k) = delimited(char('"'), alphanumeric0, char('"'))(input)?;
+fn object(input: &str) -> IResult<&str, Json> {
+    fn key_value(input: &str) -> IResult<&str, (String, Json)> {
+        let (input, key) = delimited(
+            char('"'),
+            recognize(tuple((alpha1, many0(alphanumeric1)))),
+            char('"'),
+        )(input)?;
         let (input, _) = ws_char(':')(input)?;
-        let (input, v) = alt((
-            parse_string,
-            parse_number,
-            parse_boolean,
-            parse_array,
-            parse_object,
-            parse_null,
-        ))(input)?;
-        Ok((input, (k.to_string(), v)))
+        let (input, value) = alt((string, number, boolean, array, object, null))(input)?;
+        Ok((input, (key.to_string(), value)))
     }
 
     let (input, _) = ws_char('{')(input)?;
-    let (input, key_value_list) = separated_list0(ws_char(','), parse_key_value)(input)?;
+    let (input, key_value_list) = separated_list0(ws_char(','), key_value)(input)?;
     let (input, _) = ws_char('}')(input)?;
     Ok((input, Json::Object(key_value_list.into_iter().collect())))
 }
 
-fn parse_null(input: &str) -> IResult<&str, Json> {
+fn null(input: &str) -> IResult<&str, Json> {
     let (input, _) = tag("null")(input)?;
     Ok((input, Json::Null))
 }
@@ -114,9 +98,9 @@ fn ws_char<'a>(c: char) -> impl FnMut(&'a str) -> IResult<&'a str, char> {
 impl fmt::Display for Json {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Json::String(s) => write!(f, "\"{}\"", s),
-            Json::Number(n) => write!(f, "{}", n),
-            Json::Boolean(b) => write!(f, "{}", b),
+            Json::String(value) => write!(f, "\"{}\"", value),
+            Json::Number(value) => write!(f, "{}", value),
+            Json::Boolean(value) => write!(f, "{}", value),
             Json::Array(json_list) => write!(
                 f,
                 "[{}]",
@@ -131,7 +115,7 @@ impl fmt::Display for Json {
                 "{{{}}}",
                 json_map
                     .iter()
-                    .map(|(k, v)| format!("\"{}\": {}", k, v))
+                    .map(|(key, value)| format!("\"{}\": {}", key, value))
                     .collect::<Vec<String>>()
                     .join(", ")
             ),
